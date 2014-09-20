2fcd7f7f1c2f50a65c4c0cacb984c6f98971c90e
Igor <sigorilla@gmail.com>
2014-09-20 18:05:27 +0400

    dd

--- javascripts/index_page.js
+++ javascripts/index_page.js
@@ -2,4 +2,8 @@ $(document).ready(function () {
   $(".utp").click(function () {
     // code here ...
   });
+
+  $(".onload_show").each(function (index, element) {
+    $(element).delay(200 * (index + 1)).toggleClass("hide show", 300);
+  });
 });
