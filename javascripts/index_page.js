d93a337d74d413a29dd246080e5f3fb4e3a3b8c0
Igor <sigorilla@gmail.com>
2014-09-20 18:10:01 +0400

    lets fadein

--- javascripts/index_page.js
+++ javascripts/index_page.js
@@ -3,7 +3,13 @@ $(document).ready(function () {
     // code here ...
   });
 
-  $(".onload_show").each(function (index, element) {
-    $(element).delay(200 * (index + 1)).toggleClass("hide show", 300);
-  });
+  $("onload_1").delay(1000).fadeIn(1000);
+  $("onload_2").delay(1500).fadeIn(1000);
+  $("onload_3").delay(2000).fadeIn(1000);
+  $("onload_4").delay(2500).fadeIn(1000);
+  $("onload_5").delay(3000).fadeIn(1000);
+  $("onload_6").delay(3500).fadeIn(1000);
+  $("onload_7").delay(4000).fadeIn(1000);
+  $("onload_8").delay(4500).fadeIn(1000);
+  $("onload_9").delay(5000).fadeIn(1000);
 });
